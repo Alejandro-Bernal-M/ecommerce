d09498187ebf870f23069cb9dfc9aa29c1cc7d0d
alejandro <alejandrober96@gmail.com>
2023-10-24 15:16:22 -0500

    Add database connection

--- src/index.server.js
+++ src/index.server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const env = require('dotenv');
+const mongoose = require('mongoose');
 const app = express();
 
 //env
 env.config();
+
+// database connection
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Database connected'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.status(200).json({
