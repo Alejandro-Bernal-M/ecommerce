3b7b2c24117861e39daa208b3a779263b4219455
alejandro <alejandrober96@gmail.com>
2023-10-25 13:02:28 -0500

    Add requireSignin

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin } = require('../controller/auth')
+const { signup, signin, requireSignin } = require('../controller/auth')
 
 router.post('/signin', signin);
 router.post('/signup',signup);
+router.post('/profile',requireSignin, (req, res) => {
+  res.json({message: 'profile', user: req.user})
+})
 
 
 module.exports = router;
