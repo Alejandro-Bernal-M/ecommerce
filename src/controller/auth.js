19a591cd78d07296b7c03fb13a68f6ef23490aa5
alejandro <alejandrober96@gmail.com>
2023-10-25 12:36:55 -0500

    Add signin logic

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res) => {
   const userFoundByEmail = await User.findOne({email: req.body.email});
@@ -27,5 +28,35 @@ exports.signup = async (req, res) => {
     console.log(error.errors)
     return res.status(400).json({message: 'Something went wrong', errors: error.errors})
   }
+  }
+}
+
+exports.signin = async (req, res) => {
+  
+  try {
+    const foundUser = await User.findOne({email: req.body.email});
+    if(!foundUser) return res.status(400).json({message: "Invalid email or password"});
+    if(foundUser.authenticate(req.body.password)){
+      console.log(foundUser)
+      const token = jwt.sign({_id: foundUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+      const { firstName, lastName, email, role, fullName } = foundUser;
+
+      return res.status(200).json({
+        token,
+        user: {
+          firstName,
+          lastName,
+          email,
+          role,
+          fullName
+        }
+      })
+    }else {
+      return res.status(400).json({message: "Invalid email or password"});
+    }
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({message: "Something went wrong"});
   }
 }
