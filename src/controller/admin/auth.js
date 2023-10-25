508b82fe1e4949261af9967ccb93a2d9f3248064
alejandro <alejandrober96@gmail.com>
2023-10-25 13:18:42 -0500

    Add admin routes

---
+++ src/controller/admin/auth.js
@@ -0,0 +1,69 @@
+const User = require('../../models/user')
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+  const userFoundByEmail = await User.findOne({email: req.body.email});
+  const userFoundByUsername = await User.findOne({username: req.body.username});
+
+  if(userFoundByEmail || userFoundByUsername){
+    return res.status(400).json({message: 'admin already exists'});
+  }else {
+    try{const {firstName, lastName, username, password, email} = req.body;
+    const newUser = new User({
+      firstName,
+      lastName,
+      username,
+      password,
+      email,
+      role: 'admin'
+    });
+    const savedUser = await newUser.save();
+    if(savedUser === newUser){
+      return res.json({user: savedUser});
+    }else{
+      return res.status(400).json({message: 'Error saving the admin'});
+    }
+  }catch(error){
+    console.log(error);
+    return res.status(400).json({message: 'Something went wrong', errors: error.errors})
+  }
+  }
+}
+
+exports.signin = async (req, res) => {
+  
+  try {
+    const foundUser = await User.findOne({email: req.body.email});
+    if(!foundUser) return res.status(400).json({message: "Invalid email or password"});
+    if(foundUser.authenticate(req.body.password) && foundUser.role === 'admin'){
+      console.log(foundUser)
+      const token = jwt.sign({_id: foundUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+      const {_id, firstName, lastName, email, role, fullName } = foundUser;
+
+      return res.status(200).json({
+        token,
+        user: {
+          _id,
+          firstName,
+          lastName,
+          email,
+          role,
+          fullName
+        }
+      })
+    }else {
+      return res.status(400).json({message: "Invalid email or password"});
+    }
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({message: "Something went wrong"});
+  }
+}
+
+exports.requireSignin = (req, res, next) => {
+  const token = req.headers.authorization;
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = user;
+  next();
+}
--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,7 @@ const env = require('dotenv');
 const mongoose = require('mongoose');
 const app = express();
 const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin/auth');
 
 //env
 env.config();
@@ -14,7 +15,8 @@ mongoose.connect(process.env.MONGO_URI).then(() => console.log('Database connect
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 // user route middleware
-app.use('/api', authRoutes)
+app.use('/api', authRoutes);
+app.use('/api', adminRoutes);
 
 
 
