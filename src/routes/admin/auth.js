fcd306adf71cfabdd150d857ce4e1356da13a76c
alejandro <alejandrober96@gmail.com>
2023-10-27 10:45:52 -0500

    Add admin validations

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signup, signin, requireSignin } = require('../../controller/admin/auth')
+const { validateUserRequestSignup, isValidRequest, validateUserRequestSignin }= require('../../validators/auth')
 
-router.post('/admin/signin', signin);
-router.post('/admin/signup',signup);
+router.post('/admin/signin',validateUserRequestSignin, isValidRequest, signin);
+router.post('/admin/signup',validateUserRequestSignup, isValidRequest,signup);
 router.post('/profile',requireSignin, (req, res) => {
   res.json({message: 'profile', user: req.user})
 })
