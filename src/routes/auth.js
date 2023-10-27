de7e03129f425a812bd8dbba3669498e76375d51
alejandro <alejandrober96@gmail.com>
2023-10-27 10:45:39 -0500

    Add user validations

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signup, signin, requireSignin } = require('../controller/auth')
+const { validateUserRequestSignup, isValidRequest, validateUserRequestSignin } = require('../validators/auth')
 
-router.post('/signin', signin);
-router.post('/signup',signup);
+router.post('/signin', validateUserRequestSignin, isValidRequest, signin);
+router.post('/signup',validateUserRequestSignup, isValidRequest,signup);
 router.post('/profile',requireSignin, (req, res) => {
   res.json({message: 'profile', user: req.user})
 })
