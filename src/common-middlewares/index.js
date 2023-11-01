2953443ba98a472d973b4f1c0723a3d66cead5eb
alejandro <alejandrober96@gmail.com>
2023-11-01 15:58:54 -0500

    Add admin middleware to verify rol

--- src/common-middlewares/index.js
+++ src/common-middlewares/index.js
@@ -4,5 +4,13 @@ exports.requireSignin = (req, res, next) => {
   const token = req.headers.authorization;
   const user = jwt.verify(token, process.env.JWT_SECRET);
   req.user = user;
+  console.log('user',user)
+  next();
+}
+
+exports.adminMiddleware = (req, res, next) => {
+  if(req.user.role != 'admin'){
+    return res.status(400).json({message: 'Access denied'})
+  };
   next();
 }
