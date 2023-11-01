c6f1c5b4bc6749c4b753504aaa4ad4f8dded381b
alejandro <alejandrober96@gmail.com>
2023-11-01 15:59:35 -0500

    Add middlewares to post a new category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createCategory, getCategories } = require('../controller/category')
+const { createCategory, getCategories } = require('../controller/category');
+const { requireSignin, adminMiddleware } = require('../common-middlewares');
 
-router.post('/category/create', createCategory);
+router.post('/category/create',requireSignin,adminMiddleware, createCategory);
 router.get('/categories', getCategories);
 module.exports = router;
