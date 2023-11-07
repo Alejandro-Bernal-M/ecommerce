5476c979ea2db97c9712aee23e367538a4244635
alejandro <alejandrober96@gmail.com>
2023-11-07 17:28:13 -0500

    Include cart routes in the index

--- src/index.server.js
+++ src/index.server.js
@@ -7,6 +7,7 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 //env
 env.config();
@@ -22,6 +23,7 @@ app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
 
 
 
